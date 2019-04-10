c63e7eac56ead2aa8f16f43dc3f3bf4ec200e582
GuanlinWang <43226744+GuanlinWang@users.noreply.github.com>
2019-04-09 19:26:09 -0700

    Update book.h

--- book.h
+++ book.h
@@ -1,32 +1,26 @@
-#include "book.h"
+//begin book.h
+#ifndef BOOK_H
+#define BOOK_H
 
-Book::Book(int id, string bookName, string auth, string cat) {
-    bookID=id;
-    author=auth;
-    category=cat;
-    title=bookName;
-}
-Book::Book()
-{}
+#include <string>
+#include "person.h"
 
-string Book::getTitle() {
-    return title;
-}
-
-string Book::getAuthor() {
-    return author;
-}
-
-string Book::getCategory() {
-    return category;
-}
-
-int Book::getId() {
-    return bookID;
-}
-void Book::setPersonPtr(Person * ptr) {
-} // complete
-
-Person * Book::getPersonPtr() {
-    return nullptr; // complete
-}
+class Book
+{
+private:
+    string   title;
+    string   author;
+    string   category;
+    int      bookID;
+    Person * personPtr = nullptr;
+    
+public:
+    Book(int id, string bookName, string auth, string cat);
+    string   getTitle();
+    string   getAuthor();
+    string   getCategory();
+    int      getId();
+    void     setPersonPtr(Person * ptr);
+    Person * getPersonPtr();
+};
+#endif // end book.h
